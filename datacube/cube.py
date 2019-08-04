b85958a93716dd7ba90de45b76c251f4fd121d94
Gao-Yuan <zgy0106@yeah.net>
2019-08-04 12:21:07 +0800

    rewrite _pix_trim and warnings

--- datacube/cube.py
+++ datacube/cube.py
@@ -16,6 +16,7 @@ except:
     import pyfits
 #import pywcsgrid2
 from astropy.wcs import WCS as wcs
+import warnings
 #
 #========Class Cube===================
 class Cube:
@@ -48,6 +49,8 @@ class Cube:
         Currently only velocity is used and the original fits file is assumed to
         have a velocity unit of m/s
 
+        Include index_min and index_max (or velocity min and max)
+
         Output:
             data     2D image; unit is K*km/s
             header   Try to drop all the extra header item
@@ -70,8 +73,9 @@ class Cube:
 #
 #                        sum[j,i] += \
 #                            self.cube[k,j,i]*self.header["CDELT3"]/1000.
-            sum_cube = self.cube.sum(axis=0)* \
-                  self.header["CDELT3"]/1000. # The velocity has a unit m/s
+            # include the final pix
+            sum_cube = self.cube[slice_min:slice_max+1, : , :].sum(axis=0)* \
+                self.header["CDELT3"]/1000. # The velocity has a unit m/s
 
             header_new = self.header.copy()
             try:
@@ -120,18 +124,29 @@ class Cube:
         #cent_ceiling = np.ceiling(self.header["CRPIX3"])
         #index_floor = index_list[cent_floor]
         #index_ceiling = index_list[cent_ceiling]
+        ### include the final pixel
         if (inplace):
-            new_cube = self.cube[slice_min:slice_max, :, :]
+            new_cube = self.cube[slice_min:slice_max+1, :, :]
             new_header = self.header
         else:
-            new_cube = self.cube.copy()[slice_min:slice_max, :,:]
+            new_cube = self.cube.copy()[slice_min:slice_max+1, :,:]
             new_header = self.header.copy()
 
         new_header.add_history("Trimed by pyDataCube")
         #new_header["CRVAL3"]
         #new_header["CDELT3"]
         #The central pixel should be changed
-        new_header["CRPIX3"] = _pix_trim(self.header["CRPIX3"], slice_min, slice_max)
+        with warnins.catch_warnins():
+            warnings.filterwarnings("error")
+            try:
+                # slice_min and slice_max was for python, has a 0-origin
+                new_header["CRPIX3"] = self._pix_trim(self.header["CRPIX3"], \
+                                                      slice_min+1, slice_max+1, \
+                                                      origin=1)
+            except UserWarning:
+                warnings.warn("Warning: changed central pix is not within the interval!")
+                pass
+                # here need something to do with CRVAL and CRPIX
 
         return new_cube, new_header
 
@@ -143,26 +158,45 @@ class Cube:
         elif (unit=="m/s"):
             pass
         else:
-            raise ValueError("Cube cannot read this unit")
+            raise RuntimeError("Cube cannot read this unit")
 
     def _vel2pix(self, vel_min, vel_max):
         proj = wcs(self.header)
         line_ax = ["spectral"]
         # sub is different from the original one
         line = proj.sub(line_ax)
-        slice_min = line.wcs_world2pix(vel_min, 1)
-        slice_max = line.wcs_world2pix(vel_max, 1)
+        # slice is for numpy array should use 0-origin
+        slice_min = line.wcs_world2pix(vel_min, 0)
+        slice_max = line.wcs_world2pix(vel_max, 0)
         try:
             slice_min = int(slice_min[0].round())
             slice_max = int(slice_max[0].round())
         except:
             print("The version of astropy is not satisfied!")
+            raise
 
         return (slice_min, slice_max)
 
-    def _pix_trim(d, vmin, vmax):
-        dd = (d+vmin)/(vmin+vmax)
-        return np.clip(dd, 0, 1)
+#    def _pix_trim(self, d, vmin, vmax):
+#        dd = (d+vmin)/(vmin+vmax)
+#        return np.clip(dd, 0, 1)
+     def _pix_trim(self, d, vmin, vmax, origin=1):
+         """_pix_trim
+         return a new index with the cut off edge vmin and vmax
+         d must be in beween vmin and vmax? no
+         d can be out of the range, but a warning is printed
+         origin=1 means for Fits or Fortran
+         origin=0 means for Python or C
+         """
+         if (np.isscaler(d)):
+            if (d<vmin or d>vmax):
+                warnings.warn("Warning: changed central pix is not within the interval!")
+         else:
+             if (np.logical_or(d<vmin,d>vmax).any()):
+                warnings.warn("Warning: changed central pix is not within the interval!")
+
+         dd = d-vmin+origin
+         return dd
 
 #    def channel_maps():
 
